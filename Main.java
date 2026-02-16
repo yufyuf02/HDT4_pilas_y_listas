5567b346038017698016f990b0376edf8d56bb30
yufyuf02 <che242115@uvg.edu.gt>
2026-02-15 20:40:44 -0600

    Menú funcional

--- Main.java
+++ Main.java
@@ -9,32 +9,56 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        menu();
+        int resultado = menu();
+        int seleccion = resultado / 10;
+        int opcion = resultado % 10;
+        // System.out.println(seleccion + " " + opcion);    Viendo si funciona la separación de selección y opción
     }
 
-    private static void menu() {
+// FIN DE PROGRAMA
+
+    private static int menu() {
         Scanner teclado = new Scanner(System.in);
-        int seleccion = 0;
+        int seleccion;
 
-        System.out.println("*** Calculadora de infix ***");
-        System.out.println("");
+        do {
+            System.out.println("");
+            System.out.println("*** Calculadora de infix ***");
+            System.out.println("");
 
-        if (seleccion == 0) {
-            System.out.println("Qué te gustaría usar?\r\n" + //
-                        "        (1) Pila / Stack\r\n" + //
-                        "        (2) Lista");
-            teclado.nextInt();
-        } else if (seleccion == 1) {
-            System.out.println("Qué clase de implementación te gustaría?\r\n" + //
-                        "        (1) ArrayList\r\n" + //
-                        "        (2) Vector\r\n" + //
-                        "        (3) Lista");
-        } else if (seleccion == 2) {
-            System.out.println("Qué clase de implementación te gustaría?\r\n" + //
-                        "        (1) Simplemente encadenada\r\n" + //
-                        "        (2) Doblemente encadenada");
-        }
+            System.out.println("¿Qué te gustaría usar?");
+            System.out.println("    (1) Pila / Stack");
+            System.out.println("    (2) Lista");
+            System.out.println("    (3) Salir");
 
-        System.out.println("");
+            seleccion = teclado.nextInt();
+
+            switch (seleccion) {
+                case 1:
+                    System.out.println("¿Qué clase de implementación te gustaría?");
+                    System.out.println("    (1) ArrayList");
+                    System.out.println("    (2) Vector");
+                    System.out.println("    (3) Lista");
+
+                    int opcionPila = teclado.nextInt();
+                    teclado.close();
+                    return seleccion * 10 + opcionPila;
+
+                case 2:
+                    System.out.println("¿Qué clase de implementación te gustaría?");
+                    System.out.println("    (1) Simplemente encadenada");
+                    System.out.println("    (2) Doblemente encadenada");
+
+                    int opcionLista = teclado.nextInt();
+                    teclado.close();
+                    return seleccion * 10 + opcionLista;
+                case 3:
+                    System.out.println("¡Gracias por usar! :)");
+                    teclado.close();
+                    return seleccion * 10;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        } while (true);
     }
 }
